9010c49b3cceaa491ced816f8273bcdc8aac7054
privatemails404-bob <privatemails404@gmail.com>
2026-08-10 16:10:15 +0530

    prefinal stage 3

--- birthdays/src/component/Month.tsx
+++ birthdays/src/component/Month.tsx
@@ -239,7 +239,7 @@ const deleteMemory=async(memoryIndex:number,eventIndex:number)=>{
                                     const eventIndex=props.index;
                                     deleteMemory(index,eventIndex);
                                    }}
-                                   > <svg  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></h4>
+                                   > Delete</h4>
                                    <h4 className={style.memoryDownload} 
                                    onClick={()=>{
                                     console.log("Downloading File...")
